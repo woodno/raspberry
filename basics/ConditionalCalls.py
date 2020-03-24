069283a0a9f761783dd902e8fa591a50a97542ec
Noel Wood <noel.wd@gmail.com>
2020-03-24 19:27:59 +1000

    Adding to basics

---
+++ basics/ConditionalCalls.py
@@ -0,0 +1,14 @@
+x = 101
+if x > 100:
+  print ("x is big")
+else:
+  print ("x is small")
+print ("This will always print")
+for i in range (1,11):
+  print(i)
+answer = ''
+while answer != 'X':
+  answer = input ('Enter a value X gets you out')
+  print (answer)
+print ('Finished program')
+
--- basics/StringFunctions.py
+++ basics/StringFunctions.py
@@ -2,3 +2,4 @@ s = "abcdefg"
 print (s[1:5])
 a = s.find("cd")
 print ("found at " + str(a)) 
+print ("s.upper()" + s.upper())
