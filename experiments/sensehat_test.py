f6d86643c26f3eb3b290677774e4f4e30b50cb91
Noel Wood <noel.wd@gmail.com>
2020-05-24 18:22:20 +1000

    testing

--- experiments/sensehat_test.py
+++ experiments/sensehat_test.py
@@ -5,5 +5,5 @@ blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
 while True:
-  sense.show_message("Astro Pi is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=0.01)
+  sense.show_message("Astro Pi is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=1)
 
