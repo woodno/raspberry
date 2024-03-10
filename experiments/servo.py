12f780533cf00313cbc02c2dfde34cdfa6997f44
woodno <noel.wd@gmail.com>
2024-03-11 08:04:12 +1000

    Added comments to explain servo code

--- experiments/servo.py
+++ experiments/servo.py
@@ -6,6 +6,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
+# From https://en.wikipedia.org/wiki/Servo_control#/media/File:Servomotor_Timing_Diagram.svg
+# Now for a cycle of 50 Hz this means.....
+# 
+# 1.5ms = 20ms  * x/100(where x is the duty cycle) 
+# 
+# x = 7.5% duty cycle for 0 position
+# 
+# For 90 degree position
+# 
+# 2ms = 20ms * x/100
+# 
+# x = 10% duty cycle
+# 
+# For -90 degree position
+# 
+# 1ms = 20ms * x/100
+# 
+# x = 5%
 p.start(2.5) # Initialization
 try:
   while True:
