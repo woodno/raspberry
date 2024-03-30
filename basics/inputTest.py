545c3e316c025f15f295f4b888affe9916cf2a08
woodno <noel.wd@gmail.com>
2024-03-30 14:17:02 +1000

    Exp for the need of pullup/down resistors

---
+++ basics/inputTest.py
@@ -0,0 +1,14 @@
+#Exposing random nature of pin without connections
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+testPin = 16
+GPIO.setup(testPin, GPIO.IN)
+while True:
+    if GPIO.input(channel):
+        print('Input was HIGH')
+    else:
+        print('Input was LOW')
+    
+time.sleep(0.5)
