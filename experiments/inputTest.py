2f925d65f28315306ea899ff66f99c986791c0d3
woodno <noel.wd@gmail.com>
2024-03-30 14:23:45 +1000

    Fixed inputTest code

--- experiments/inputTest.py
+++ experiments/inputTest.py
@@ -6,9 +6,8 @@ GPIO.setmode(GPIO.BCM)
 testPin = 16
 GPIO.setup(testPin, GPIO.IN)
 while True:
-    if GPIO.input(channel):
+    if GPIO.input(testPin):
         print('Input was HIGH')
     else:
         print('Input was LOW')
-    
-time.sleep(0.5)
+    time.sleep(0.5)
