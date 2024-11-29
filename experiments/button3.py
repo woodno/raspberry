537728284f2bd44f7bfc5a511a820e97fb9bf376
woodno <noel.wd@gmail.com>
2024-11-29 14:34:28 +1000

    Attempt 3

--- experiments/button3.py
+++ experiments/button3.py
@@ -12,13 +12,21 @@ GPIO.setup(PIN, GPIO.IN)
 
 
 
+# try:
+#     while (True):
+#         while (GPIO.input(PIN) == GPIO.LOW):
+#             time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
+#     i = i + 1
+#     print ("Button press "+ str(i))
+#     time.sleep(0.01)
+
 try:
-    while (True):
-        while (GPIO.input(PIN) == GPIO.LOW):
-            time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
-    i = i + 1
-    print ("Button press "+ str(i))
-    time.sleep(0.01)
+    while(True):
+        if (GPIO.input(PIN) == GPIO.LOW):
+            print ("Low")
+        else:
+            print ("High")
+        time.sleep(0.01)
         
        
 finally:
