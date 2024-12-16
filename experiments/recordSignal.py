8c26cccc5cc3606dd6b1c36331e7dd46fedf3642
woodno <noel.wd@gmail.com>
2024-12-16 13:49:52 +1000

    Test radio signal

---
+++ experiments/recordSignal.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+PIN = 18
+
+
+
+GPIO.setup(PIN, GPIO.IN)
+#If using internal pull up resistor include pull_up_down = GPIO.PUD_DOWN
+# eg GPIO.setup(PIN, GPIO.IN , pull_up_down = GPIO.PUD_DOWN)
+
+
+
+try:
+    while (True):
+        print (GPIO.input (PIN))
+        time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
+        
+
+finally:
+    print ("Cleaning Up")
+    GPIO.cleanup()
+    print ("Done Cleaning Up")
