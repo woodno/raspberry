9d0da325794d75a6ca288047abcbc0d4b5762c81
woodno <noel.wd@gmail.com>
2025-02-26 15:29:23 +1000

    Function calls added

--- basics/functions2025.py
+++ basics/functions2025.py
@@ -1,13 +1,15 @@
 #Work out the area of circles from 1 to 100 radius in increments of 1
+pi = 3.14
 def area (radius):
     #The following line is to teach about the scope of a variable
-    #pi = 3.14 
-    return pi * radius * radius
+    #pi = 3.14
+    areaans = pi * radius  ** 2
+    return areaans
 
 def circum (radius):
     return 2 * pi * radius
 
-pi = 3.14
+
 for i in range (101):
     print ("The area of a circle with radius " + str(i) + " is "  + str(area (i)))
     print ("The circumfrance of a circle with radius " + str(i) + " is "  + str(circum (i)))
---
+++ experiments/BrianHwk2.py
@@ -0,0 +1,16 @@
+#Brian Homework code
+mycounter = 1
+
+ 
+
+while (mycounter <101) :
+
+    radius_s = mycounter
+
+    radius = float(radius_s)
+
+    circum4 = 2 * 3.14159 * radius
+
+    print ("Using a radius of " +str(mycounter) +( " and using 3.14159 pi the circumference is  ") +str(circum4))
+
+    mycounter = mycounter +1
