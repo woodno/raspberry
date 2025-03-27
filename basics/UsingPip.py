c42ea568167b1fed7f86ba2545305c9582c0b18d
woodno <noel.wd@gmail.com>
2025-03-27 22:18:36 +1000

    Work for week 9

---
+++ basics/UsingPip.py
@@ -0,0 +1,24 @@
+Todays coding is modules and pip
+
+
+
+mkdir my_vir
+cd my_vir
+python -m venv --system-site-packages env
+source env/bin/activate
+
+pip3 install mcp3008
+
+pip3 list
+
+https://pypi.org/project/mcp3008/
+
+pip3 show mcp3008
+
+Find more packages at https://pypi.org/
+
+using  import and import xxx as yyy and
+
+from xxx import yyy as www
+
+using with closes some things after use usually files
