35ecfa15d6c9aa1368dc60e28349a11c6f26979c
woodno <noel.wd@gmail.com>
2025-04-01 12:24:44 +1000

    Added comments to explain toggleLed.  Cleaned up some dashes in adc_test2 to make sure they did not look like negatives

--- experiments/adc_test2.py
+++ experiments/adc_test2.py
@@ -5,18 +5,19 @@ from gpiozero import MCP3008
 #doco at https://gpiozero.readthedocs.io/en/stable/api_spi.html
 #Clockwise from top right
 #Pin 16 VDD - 3.3 V
-#Pin 15 VREF -3.3 V
+#Pin 15 VREF - 3.3 V
 #Pin 14 AGND -GND
 #Pin 13 CLK - SCLK
 #Pin 12 DOUT - MISO
-#Pin 11 DIn -MOSI
-#Pin 10 CS/SHDN -CE0
+#Pin 11 DIn - MOSI
+#Pin 10 CS/SHDN - CE0
 #Pin 9 DGND - GND
 #Pin 1 Ch 0 (Top left pin) - Analog input being measured.
 #For the test a 3.3 V input was output to an outside pin of a pot
 #The middle pin of the pot was connected to the Channel 0 input
 #The other outside pin was put to a ground.
 #A voltage divider should be used to ensure the voltage is not exceeded.
+#In our case the max input is 3.3 V so we don't need a voltage divider.
 
 import time
 analog_input = MCP3008(channel=0,clock_pin=11,mosi_pin=10,
--- experiments/toggleLed.py
+++ experiments/toggleLed.py
@@ -18,25 +18,24 @@ try:
     while (True):
         while (GPIO.input(PIN) == GPIO.LOW):
             time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
+        #If we are here Pin 18 is no longer in the low position
+        #which means the button has been pressed.
         i = i + 1
         print ("Button press "+ str(i))
+        #First time through isLedOn is false so it is off
+        #This if says if the led is off tiurn it on then change the flag to true
         if not isLedOn:
             GPIO.output(LEDPIN, True)
             isLedOn = True
+        #This else says if the led is on turn it off and set the flag to false.    
         else:
             GPIO.output(LEDPIN, False)
             isLedOn = False
+        #This while loop is here to make sure each press of the switch is recorded as only one press
+        #Without this if you pushed the button down for more than 0.01 seconds it would record multiple
+        #pushes of the switch and hence get unpredictable results.
         while (GPIO.input(PIN) == GPIO.HIGH):
             time.sleep(0.01)
-
-# try:
-#     while(True):
-#         if (GPIO.input(PIN) == GPIO.LOW):
-#             print ("Low")
-#         else:
-#             print ("High")
-#         time.sleep(0.01)
-#         
        
 finally:
     print ("Cleaning Up")
