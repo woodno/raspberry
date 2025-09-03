6e792154c1b1f664d3f6f42c46a790cc5a48c742
woodno <noel.wd@gmail.com>
2025-09-03 19:41:17 +1000

    Week 3 code done

--- basics/variables.py
+++ basics/variables.py
@@ -1,14 +1,35 @@
-#Variables, datatypes, comments
+#Variables, datatypes, comments, operations, casting
 #This is a comment 
 print ("Starting program")
 #variables
 r = "5"
 
+#Constants are not special variables in Pi
+#They are named in capitals
+#They go at thye start of a program or block
+MAX_VOLTAGE = 3.3
+
+
 print ("The value of r is " + r)
+print ("The type of r is " , type(r))
 
 x = 7
 print ("the value of x is " + str(x))
+print ("The type of x is " , type(x))
+c  = 7.0
+print ("the value of c is " + str(c))
+print ("The type of c is " , type(c))
+#Make a new variable z from casting r to a float.
+#Print it out and check its type
 
-#init a var caalled y make it equal 25
-#init a variable called u make it 6
-# print out y+u, y-u , y*u and y/u
+#Arithmetic Operators
+#init a var called y make it equal 2
+#init a variable called u make it 16
+# print out y+u, y-u , y*u,  y**u,  and u/y
+
+#init a var called t make it equal 25
+#init a variable called n make it 7
+
+#print out t%n and t//n
+#what do you think these operators do?
+
---
+++ pico_code/led_pwm.py
@@ -0,0 +1,40 @@
+from machine import Pin, PWM # we need to import PWM to use PWM
+import time
+# intialise pin 1 as a PWM output
+pwm_pin = PWM(Pin(1))
+# this sets up the frequency that the pin is turned off and on (it is not duty cycle)
+# all constants should be set at the beginning of the program
+# later on we will learn how to do this.
+pwm_pin.freq(1000)
+
+# this varaible is used to help calculate the required input from a duty cycle percentage
+# the value for the duty cycle is held as 2 bytes of data.
+# 2^16 = 65536. 
+max = 65535
+
+try:
+    while True:
+        # here we multiply max by the desired duty cycle (as a decimal), 0.2 is 20%, 0.7 is 70% and so on
+        # later on we will do calculations like this in a function
+        PWM_value = int(0.2 * max)
+        # this line is the command that actually sets the duty cycle
+        pwm_pin.duty_u16(PWM_value)
+        print("My LED is 20% bright")
+        time.sleep (2)
+        PWM_value = int(0.5 * max)
+        pwm_pin.duty_u16(PWM_value)
+        print("My LED is 50% bright")
+        time.sleep (2)
+        PWM_value = int(0.75 * max)
+        pwm_pin.duty_u16(PWM_value)
+        print("My LED is 75% bright")
+        time.sleep (2)
+        PWM_value = int(1 * max)
+        pwm_pin.duty_u16(PWM_value)
+        print("My LED is 100% bright")
+        time.sleep (2)
+finally:
+    pwm_pin.deinit()
+    input_pin = (1, Pin.IN)
+    print ("I've cleaned up")
+    
